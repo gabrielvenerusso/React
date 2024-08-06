1dafc07e906b5ebf063f96758347aefb27bcab11
Gabriel Mendes Venerusso <gabriel.venerusso@martinellilabs.com.br>
2024-08-06 10:11:14 -0300

    tentativa

---
+++ Avancando/fundamentosavancados/src/components/ManageData.js
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+
+const ManageData = () =>{
+    let someData = 10;
+    
+    let [number, setNumber] = useState(12);
+
+    return(
+        <div>
+            <div>
+                <p>Valor : {someData}</p>
+                <button onClick ={() => someData = 15}>Mudar valor</button>
+            </div>
+            <div>
+                <p>Valor : {number}</p>
+                <button onClick ={() => setNumber(14)}>Mudar valor</button>
+            </div>
+        </div>        
+    );
+};
+
+export default ManageData;
