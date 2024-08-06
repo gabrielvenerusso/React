6b95017a38608efc936c9949ace85b3bf7600bca
Gabriel Venerusso <98406849+gabrielvenerusso@users.noreply.github.com>
2024-08-06 15:21:56 -0300

    Add files via upload

--- Avancando/fundamentosavancados/src/App.js
+++ Avancando/fundamentosavancados/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Lua from "./assets/preview.gif";
 import MudarBotao from "./components/ManageData"
+import Listas from "./components/ListRender"
+import Render from "./components/ConditionalRender"
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  const name = "Gabriel";
   return (
     <div className="App">
       <div>
         <img src={Lua}/>
         <MudarBotao/>
+        <Listas/>
+        <Render/>
+        <ShowUserName name={name}/>
+        <CarDetails brand="VW" km={1000} color="azul"/>
       </div>       
     </div>
   );
